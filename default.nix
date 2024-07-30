1e21a5283b033f004a5f5e4858719d30b4e115a1
mxmf <2993747433@qq.com>
2024-07-30 15:20:06 +0800

    add easytier

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  easytier = pkgs.callPackage ./pkgs/easytier { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
