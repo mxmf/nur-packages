d3f9685c5ecf071924725dc1b45c8a633542b8ac
mxmf <2993747433@qq.com>
2025-03-07 19:06:44 +0800

    update touying

--- pkgs/touying/default.nix
+++ pkgs/touying/default.nix
@@ -1,80 +1,27 @@
 { lib
 , setuptools
 , fetchPypi
-, maturin
-, pipInstallHook
-, rustPlatform
 , python-pptx
+, python3Packages
 , pillow
 , jinja2
-, python3
-
 , buildPythonPackage
 }:
 
-let
-  typst-py = rustPlatform.buildRustPackage rec{
-    pname = "typst";
-    version = "0.11.1";
-
-    src = fetchPypi {
-      inherit pname version;
-      hash = "sha256-VqM2/vHb7GrmeNh9aAqQrQfIc5vAWZtU/df6NRpO+lY=";
-    };
-
-    nativeBuildInputs = [ maturin pipInstallHook ];
-
-    propagatedBuildInputs = [ python3 ];
-
-    doCheck = false;
-
-    cargoLock =
-      {
-        lockFile = ./Cargo.lock;
-      };
-
-    # In order to make typst-lsp build with rust >= 1.80,  use the patched Cargo.lock and Cargo.toml
-    postPatch = ''
-      rm Cargo.lock Cargo.toml
-      ln -s ${./Cargo.lock} Cargo.lock
-      ln -s ${./Cargo.toml} Cargo.toml
-    '';
-
-    buildPhase = ''
-      maturin build --release --manylinux off
-    '';
-
-    installPhase = ''
-      # Put the wheels where the pip install hook can find them.
-      install -Dm644 -t dist target/wheels/*.whl
-      pipInstallPhase
-    '';
-
-    meta = with lib;{
-      description = "Python binding to typst, a new markup-based typesetting system that is powerful and easy to learn.";
-      homepage = "https://pypi.org/project/typst/";
-      license = licenses.asl20;
-      maintainers = with maintainers; [ mxmf ];
-    };
-  };
-in
 buildPythonPackage rec{
   pname = "touying";
-  version = "0.11.3";
+  version = "0.12.1";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ShHB6rJiTDJDzFRL32o1OfwCw+XnA9+1vuo7vbNvZWM=";
+    hash = "sha256-sCoUzZLVsydTfonJhsbOKy3Ntk022x6X8INHlJamJjQ=";
   };
 
   propagatedBuildInputs = [
-    # ...
     setuptools
   ];
-  # pyproject = true;
-  format = "pyproject";
 
-  # format = "setuptools";
+  format = "pyproject";
 
   prePatch = ''
     sed -i '/argparse/d' pyproject.toml
@@ -84,7 +31,7 @@ buildPythonPackage rec{
     python-pptx
     pillow
     jinja2
-    typst-py
+    python3Packages.typst
   ];
 
   meta = with lib;{
